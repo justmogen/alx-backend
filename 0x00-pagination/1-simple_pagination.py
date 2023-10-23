7e9878542b311abb9c57e302b55f4318c0833543
justmogen <justmogen@gmail.com>
2023-10-24 00:11:35 +0300

    getting the contents of the csv file before paginate it

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 """ Simple helper function """
 from typing import Tuple
-import csv
-from typing import List
-import math
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ Simple helper function """
     return ((page - 1) * page_size, page * page_size)
+
+
+
+import csv
+from typing import List
+import math
+
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -30,9 +35,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            pass
-            assert type(page) == int > 0
-            assert type(page_size) == int > 0
-            self.dataset()
+            """ Retrieve a specific page from dataset """
+            assert isinstance(page, int) and page > 0, "page should be a positive integer"
+            assert isinstance(page_size, int) and page_size > 0, "page_size should be a positive integer"
+
             start, end = index_range(page, page_size)
-            return self.__dataset[start:end]
+            dataset = self.dataset()
+
+            if start >= len(dataset):
+                return [] # If the start index is greater or equal to the
+            else:
+                return dataset[start:end]
