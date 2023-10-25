6704b47a1773c3243c876feb137eacbc158dc998
justmogen <justmogen@gmail.com>
2023-10-25 11:53:25 +0300

    LFU  and LRU

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -4,37 +4,43 @@ from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """LFUCache class with LRU algorithm"""
+    """LFUCache class with LFU algorithm"""
 
     def __init__(self):
         """constructor"""
         super().__init__()
-        self.queue = []
-        self.count = {}
+        self.frequencies = {}  # Dictionary to store frequencies of keys
 
     def put(self, key, item):
-        """remove least frequently used eleement """
+        """add an item in cache with LFU eviction policy"""
         if key and item:
             if key in self.cache_data:
+                # Update the item and frequency
                 self.cache_data[key] = item
-                self.queue.remove(key)
-                self.queue.append(key)
-                self.count[key] += 1
-                return
-            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                discard = self.queue.pop(0)
-                del self.cache_data[discard]
-                del self.count[discard]
-                print("DISCARD:", discard)
-            self.queue.append(key)
-            self.cache_data[key] = item
-            self.count[key] = 1
+                self.frequencies[key] += 1
+            else:
+                # Add the new item and set its frequency to 1
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    # Evict the least frequently used item(s)
+                    min_frequency = min(self.frequencies.values())
+                    keys_to_remove = [
+                        k for k, v in self.frequencies.items() if v == min_frequency]
+                    # Remove the first item that was least frequently used
+                    discard = keys_to_remove[0]
+                    del self.cache_data[discard]
+                    del self.frequencies[discard]
+                    print("DISCARD:", discard)
+                self.cache_data[key] = item
+                self.frequencies[key] = 1
 
     def get(self, key):
-        """return the value linked to the key"""
+        """return the value linked to key"""
         if key in self.cache_data:
-            self.queue.remove(key)
-            self.queue.append(key)
-            self.count[key] += 1
+            # Update the frequency
+            self.frequencies[key] += 1
             return self.cache_data[key]
         return None
+
+    def cache_info(self):
+        """return cache information"""
+        return self.cache_data, self.frequencies
