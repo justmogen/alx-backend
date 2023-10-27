043251db4df142870752cbdd32820b81add664b9
justmogen <justmogen@gmail.com>
2023-10-27 18:27:04 +0300

    more concise code correction

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
+"""Deletion-resilient hypermedia pagination
 """
-Deletion-resilient hypermedia pagination
-"""
-
 import csv
-import math
-from typing import List
+from typing import Dict, List
 
 
 class Server:
@@ -14,6 +11,8 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
         self.__indexed_dataset = None
 
@@ -28,7 +27,7 @@ class Server:
 
         return self.__dataset
 
-    def indexed_dataset(self) -> dict[int, List]:
+    def indexed_dataset(self) -> Dict[int, List]:
         """Dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
@@ -39,25 +38,29 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
-        """ Retrieve info about a specific page from a given index
-            and with a specific size
-         """
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Retrieves info about a page from a given index and with a
+        specified size.
+        """
         data = self.indexed_dataset()
-        assert index is not None and 0 <= index <= max(data.keys())
-
+        assert index is not None and index >= 0 and index <= max(data.keys())
         page_data = []
+        data_count = 0
         next_index = None
-        for i, item in data.items():
-            if i >= index:
-                page_data.append(item)
-                if len(page_data) >= page_size:
-                    next_index = i
-                    break
+        start = index if index else 0
 
-        return {
+        for i, item in data.items():
+            if i >= start and data_count < page_size:
+                page_data.append(item)
+                data_count += 1
+                continue
+            if data_count == page_size:
+                next_index = i
+                break
+        page_info = {
             'index': index,
-            'data': page_data,
+            'next_index': next_index,
             'page_size': len(page_data),
-            'next_index': next_index
+            'data': page_data,
         }
+        return page_info
