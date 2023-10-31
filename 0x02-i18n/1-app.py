89c0e65dec2b77f6d578dc40710db166d361b3e2
justmogen <justmogen@gmail.com>
2023-10-31 15:41:44 +0300

    basic flask configuration apps

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+from flask import Flask, render_template
+from flask_babel import Babel
+from config import Config
+
+
+app = Flask(__name__)
+app.config.from_object(Config) # Config class for app.config
+babel = Babel(app) # Babel instance for babel
+
+@babel.localeselector
+def get_locale():
+    """ Get locale language """
+    return 'fr'
+
+@app.route('/')
+def index():
+    """ Index page """
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
---
+++ 0x02-i18n/config.py
@@ -0,0 +1,5 @@
+class Config:
+    """ Config class """
+    Languages = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
